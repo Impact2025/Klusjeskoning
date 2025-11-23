d85329d56d63caab468152370b3e686310443e75
Impact2025 <chat@weareimpact.nl>
2025-11-23 21:09:35 +0100

    Fix CSP to allow Google Analytics region1 subdomain

--- next.config.ts
+++ next.config.ts
@@ -53,7 +53,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https: blob:",
-              "connect-src 'self' https://www.google-analytics.com https://clarity.microsoft.com https://api.qrserver.com https://weareimpact.nl",
+              "connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://clarity.microsoft.com https://api.qrserver.com https://weareimpact.nl",
               "frame-src 'none'",
               "object-src 'none'",
               "base-uri 'self'",
